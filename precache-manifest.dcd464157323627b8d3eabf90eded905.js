22d6d708694b62bb5dfeff3de75ad657560242f8
henry <henry.argueta@aol.com>
2020-02-03 16:14:59 -0500

    Updates

---
+++ precache-manifest.dcd464157323627b8d3eabf90eded905.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df1a80005241eb7471fb5e491c7365d4",
+    "url": "/tictac/index.html"
+  },
+  {
+    "revision": "dafc31434a7e143777da",
+    "url": "/tictac/static/css/main.e01d0681.chunk.css"
+  },
+  {
+    "revision": "857caae6af315a0e9dbc",
+    "url": "/tictac/static/js/2.7cbbabf5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tictac/static/js/2.7cbbabf5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dafc31434a7e143777da",
+    "url": "/tictac/static/js/main.aa4759af.chunk.js"
+  },
+  {
+    "revision": "eadbf01a0b1fff4979f1",
+    "url": "/tictac/static/js/runtime-main.d4860815.js"
+  }
+]);
